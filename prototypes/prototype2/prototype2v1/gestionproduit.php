bc022da9e9d848246991cb51a44eea1274ff8aac
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2022-03-28 13:29:55 +0100

    added

--- prototypes/prototype2/prototype2v1/gestionproduit.php
+++ prototypes/prototype2/prototype2v1/gestionproduit.php
@@ -6,8 +6,15 @@
  class GestionProduit {
      public $name ;
      private $Connection = Null;
-     
- {
+     private function getConnection()
+     {
+         $this->Connection = mysqli_connect('localhost', 'admin', 'admin', 'site-e-commerce');
+         if (!$this->Connection) {
+             die('Could not connect to database server');
+         }
+         return $this->Connection; 
+    }
+    
      
  
 
