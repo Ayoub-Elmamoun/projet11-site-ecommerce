1ca8ae80270e4c045be978012562c2cd37d274d3
Ayoub-Elmamoun <saadcrax@protonmail.com>
2022-04-06 10:59:24 +0100

    added

---
+++ Realisation/creatdb.php
@@ -0,0 +1,23 @@
+<?php
+
+class CreatDb {
+
+    public $servername;
+    public $username;
+    public $password;
+    public $tablename;
+    public $dbname;
+    public $connection;
+
+    // constructor class
+    public function Constructor()
+    {
+        $dbname = "NewDb";
+        $servername = "localhost";
+        $tablename = "ProuctDb"
+        
+    }
+}
+
+
+?>
