c244e25974731a402e4ca6de34c8302b1f47535d
Ayoub-Elmamoun <saadcrax@protonmail.com>
2022-04-06 11:20:59 +0100

    creat database done !

--- Realisation/creatdb.php
+++ Realisation/creatdb.php
@@ -1,21 +1,32 @@
 <?php
 
-class CreatDb {
+class creatdb {
 
     public $servername;
     public $username;
-    public $password;
+    public $user_password;
     public $tablename;
     public $dbname;
     public $connection;
 
-    // constructor class
-    public function Constructor()
+        // constructor class
+    public function Constructor($dbname = "NewDb",$servername = "localhost",$tablename = "Productdb",$username = "admin",$user_password = "")
     {
-        $dbname = "NewDb";
-        $servername = "localhost";
-        $tablename = "ProuctDb"
-        
+        // initializing the shit 
+
+        $this->dbname =$dbname;
+        $this->servername = $servername;
+        $this->tablename =$tablename;
+        $this->username =$username;
+        $this->user_pasword =$user_password;
+
+        // creat connction and check it 
+
+        $this->connection = mysqli_connect($servername,$username,$user_password);
+        if (!$this->connection) 
+            die ("connection failed".mysli_connection_error());
+        }
+
     }
 }
 
