a42c4e3eab7fbc0bfd65ee2ae8e793f89563374d
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2022-04-26 11:21:48 +0100

    index created

---
+++ livecoding/config/config.php
@@ -0,0 +1,19 @@
+<?php 
+
+class dataBase{
+
+    function connectDb(){
+
+        $connect=null;
+        if ($connect == null) {
+
+            // try to connect to database
+
+            $connect = myslqi_connect('localhost','root','', 'livecoding');
+        }
+        else {
+            echo 'cannot connect buddy';
+        }
+        return $connect;
+    };
+?>
