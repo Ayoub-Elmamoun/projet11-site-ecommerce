762f858c9049a94adb7fb788766349425a2f1dc7
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2022-04-29 13:32:18 +0100

    added

--- oop_mession/class_thing.php
+++ oop_mession/class_thing.php
@@ -10,4 +10,21 @@ class product_class
 $product = new product_class();
 
 $product->product_name = "Apple";
-echo $product_name;
+$product_name = $product->product_name;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1><? echo $product_name;?> </h1>
+</body>
+
+</html>
